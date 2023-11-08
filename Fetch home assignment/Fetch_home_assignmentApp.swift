2f90c6eed0636935297f69c838187e38fbff9728
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-11-08 08:26:04 -0700

    Initial Commit

---
+++ Fetch home assignment/Fetch_home_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_home_assignmentApp.swift
+//  Fetch home assignment
+//
+//  Created by Ismatulla Mansurov on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_home_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
